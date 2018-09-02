d0f520a3b14c29c280b95a7610a3d99e2dbfe1b6
Ramon <emprendedorrgv@gmail.com>
2018-09-01 23:34:10 -0600

    Se agrego el nav del usuario, y las cartas de los alojamientos

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -1,4 +1,12 @@
-
+// iniciacion del parallax
  $(document).ready(function(){
    $('.parallax').parallax();
  });
+ // iniciacion del nab- de la izquierda
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  // iniciacion button floating
+  $(document).ready(function(){
+     $('.fixed-action-btn').floatingActionButton();
+   });
