1916b3a4620afcf7c467d86cab508c74851e54e5
Ramon <emprendedorrgv@gmail.com>
2018-09-01 22:12:25 -0600

    Se agrego la seccion de inicio y la secion de nuestros servicios, al igual que el nav superior

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -1,13 +1,4 @@
 
-// js parrallax
-
-// document.addEventListener('DOMContentLoaded', function() {
-//    var elems = document.querySelectorAll('.parallax');
-//    var instances = M.Parallax.init(elems, options);
-//  });
-
- // Or with jQuery
-
  $(document).ready(function(){
    $('.parallax').parallax();
  });
